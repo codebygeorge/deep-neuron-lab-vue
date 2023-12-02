d55f241e5ec0b28574a764cec205ed337e097566
George <code.by.george@gmail.com>
2023-12-03 00:38:11 +0400

    - Removed unused files - Added env file - Added global styles - Added common components

---
+++ src/components/common/Checkbox.vue
@@ -0,0 +1,117 @@
+<script lang="ts">
+import { defineComponent, PropType, InputHTMLAttributes } from "vue";
+
+export default defineComponent({
+  props: {
+    inputProps: {
+      type: Object as PropType<InputHTMLAttributes>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <label class="mcui-checkbox">
+    <input type="checkbox" v-bind="inputProps" />
+    <div>
+      <svg class="mcui-check" viewBox="-2 -2 35 35" aria-hidden="true">
+        <title>checkmark</title>
+        <polyline points="7.57 15.87 12.62 21.07 23.43 9.93" />
+      </svg>
+    </div>
+  </label>
+</template>
+
+<style scoped>
+.mcui-checkbox {
+  display: flex;
+  align-items: center;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  padding: 0.25em 1em;
+  box-sizing: border-box;
+  cursor: pointer;
+}
+.mcui-checkbox input[type="checkbox"] {
+  position: absolute !important;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px, 1px, 1px, 1px);
+}
+.mcui-checkbox input[type="checkbox"] + div {
+  border: 2px solid white;
+  /*background-color: rgba(255, 255, 255, 0.34);*/
+  height: 1.5em;
+  width: 1.5em;
+  box-sizing: border-box;
+  border-radius: 100%;
+  position: relative;
+}
+
+.mcui-checkbox input[type="checkbox"]:checked + div {
+  background-color: white;
+  transition: border-color 107ms cubic-bezier(0.65, 0.25, 0.56, 0.96);
+}
+.mcui-checkbox input[type="checkbox"]:checked + div .mcui-check {
+  opacity: 1;
+  transition: opacity 107ms cubic-bezier(0.65, 0.25, 0.56, 0.96);
+}
+.mcui-checkbox input[type="checkbox"]:checked + div .mcui-check polyline {
+  -webkit-animation: dash-check 107ms cubic-bezier(0.65, 0.25, 0.56, 0.96)
+    forwards;
+  animation: dash-check 107ms cubic-bezier(0.65, 0.25, 0.56, 0.96) forwards;
+}
+.mcui-checkbox input[type="checkbox"]:indeterminate + div::after {
+  content: "";
+  height: 4px;
+  width: 60%;
+  left: 20%;
+  top: calc(50% - 2px);
+  position: absolute;
+  background: #c4c6cb;
+  border-radius: 1px;
+}
+.mcui-checkbox input[type="checkbox"]:disabled ~ div {
+  opacity: 0.6;
+  color: #c4c6cb;
+  cursor: not-allowed;
+}
+.mcui-checkbox input[type="checkbox"]:enabled ~ div {
+  cursor: pointer;
+}
+
+.mcui-check {
+  height: 100%;
+  width: 100%;
+  transform: scale(1);
+  color: limegreen;
+  opacity: 0;
+}
+.mcui-check polyline {
+  fill: none;
+  transform-origin: 50% 50%;
+  stroke-width: 6px;
+  stroke-dasharray: 22.7713679002;
+  stroke: currentcolor;
+}
+
+@supports (display: grid) {
+  .mcui-check polyline {
+    stroke-dashoffset: 22.7713679002;
+  }
+}
+@-webkit-keyframes dash-check {
+  to {
+    stroke-dashoffset: 0;
+  }
+}
+@keyframes dash-check {
+  to {
+    stroke-dashoffset: 0;
+  }
+}
+</style>
